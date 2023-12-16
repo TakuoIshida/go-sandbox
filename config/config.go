ce3055ce6dfff4392c4bc291d2d2458c864e9502
Tishida <archersparadox.100@gmail.com>
2023-12-16 16:17:21 +0900

    localとcloudのsql接続を場合分け

--- config/config.go
+++ config/config.go
@@ -8,30 +8,33 @@ import (
 )
 
 type Config struct {
-	GIN_MODE   string
-	DbHost     string
-	DbUser     string
-	DbName     string
-	DbPassword string
-	DbPort     string
+	GIN_MODE                 string
+	DbHost                   string
+	DbUser                   string
+	DbName                   string
+	DbPassword               string
+	DbPort                   string
+	INSTANCE_CONNECTION_NAME string
+	PRIVATE_IP               string
 }
 
 var Conf Config
 
 func LoadConfig() {
-	err := godotenv.Load("./db/.env.local")
+	err := godotenv.Load("./db/.env")
 	if err != nil {
 		// TODO localのみ。dev, productionでは、読み込まない
 		log.Fatalf("Error loading .env file: %v", err)
 	}
 
 	Conf = Config{
-		GIN_MODE:   os.Getenv("GIN_MODE"),
-		DbHost:     os.Getenv("POSTGRES_HOST"),
-		DbUser:     os.Getenv("POSTGRES_USER"),
-		DbName:     os.Getenv("POSTGRES_DB"),
-		DbPassword: os.Getenv("POSTGRES_PASSWORD"),
-		DbPort:     os.Getenv("POSTGRES_PORT"),
+		GIN_MODE:                 os.Getenv("GIN_MODE"),
+		DbHost:                   os.Getenv("POSTGRES_HOST"),
+		DbUser:                   os.Getenv("POSTGRES_USER"),
+		DbName:                   os.Getenv("POSTGRES_NAME"),
+		DbPassword:               os.Getenv("POSTGRES_PASSWORD"),
+		DbPort:                   os.Getenv("POSTGRES_PORT"),
+		INSTANCE_CONNECTION_NAME: os.Getenv("INSTANCE_CONNECTION_NAME"),
+		PRIVATE_IP:               os.Getenv("PRIVATE_IP"),
 	}
-
 }
--- infrastructure/database/database.go
+++ infrastructure/database/database.go
@@ -1,10 +1,18 @@
 package database
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
 	"go-sandbox/config"
 	"go-sandbox/query"
+	"log"
+	"net"
 
+	"cloud.google.com/go/cloudsqlconn"
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v4"
+	"github.com/jackc/pgx/v4/stdlib"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -14,8 +22,28 @@ type DBClientConnector struct {
 }
 
 func NewDBClientConnector() *DBClientConnector {
-	cfg := config.Conf
+	if config.Conf.GIN_MODE == gin.ReleaseMode || config.Conf.GIN_MODE == gin.TestMode {
+		db, err := connectWithCloudSql()
+		if err != nil {
+			log.Fatalf("cannot connect with local db")
+		}
+		return &DBClientConnector{
+			DB: db,
+		}
+	} else {
+		// local: config.Conf.GIN_MODE == gin.DebugMode
+		db, err := connectWithLocalDB()
+		if err != nil {
+			log.Fatalf("cannot connect with local db")
+		}
+		return &DBClientConnector{
+			DB: db,
+		}
+	}
+}
 
+func connectWithLocalDB() (*gorm.DB, error) {
+	cfg := config.Conf
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Tokyo", cfg.DbHost, cfg.DbUser, cfg.DbPassword, cfg.DbName, cfg.DbPort)
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
@@ -27,8 +55,46 @@ func NewDBClientConnector() *DBClientConnector {
 	}
 	query.SetDefault(db)
 	fmt.Println("SetDefault!!")
+	return db, err
+}
 
-	return &DBClientConnector{
-		DB: db,
+func connectWithCloudSql() (*gorm.DB, error) {
+	usePrivate := config.Conf.PRIVATE_IP
+	instanceConnectionName := config.Conf.INSTANCE_CONNECTION_NAME
+
+	dsn := fmt.Sprintf("user=%s password=%s database=%s", config.Conf.DbUser, config.Conf.DbPassword, config.Conf.DbName)
+	connConfig, err := pgx.ParseConfig(dsn) //NOTE: pgxはpostgresql用のDriver
+	if err != nil {
+		return nil, err
 	}
+	connConfig.DialFunc = func(ctx context.Context, network, instance string) (net.Conn, error) {
+		if usePrivate != "" {
+			d, err := cloudsqlconn.NewDialer(
+				ctx,
+				cloudsqlconn.WithDefaultDialOptions(cloudsqlconn.WithPrivateIP()),
+			)
+			if err != nil {
+				return nil, err
+			}
+			return d.Dial(ctx, instanceConnectionName)
+		}
+		// Use the Cloud SQL connector to handle connecting to the instance.
+		// This approach does *NOT* require the Cloud SQL proxy.
+		d, err := cloudsqlconn.NewDialer(ctx)
+		if err != nil {
+			return nil, err
+		}
+		return d.Dial(ctx, instanceConnectionName)
+	}
+	dbURI := stdlib.RegisterConnConfig(connConfig)
+	dbPool, err := sql.Open("pgx", dbURI)
+	if err != nil {
+		return nil, fmt.Errorf("sql.Open: %v", err)
+	}
+
+	gormDB, err := gorm.Open(postgres.New(postgres.Config{
+		Conn: dbPool,
+	}), &gorm.Config{})
+
+	return gormDB, nil
 }
