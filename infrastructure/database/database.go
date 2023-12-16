45fb3e2231e49a8783b755fa0939f8d4ff13cd60
Tishida <archersparadox.100@gmail.com>
2023-12-16 17:23:38 +0900

    dbのconnection設定を修正

--- infrastructure/database/database.go
+++ infrastructure/database/database.go
@@ -1,18 +1,13 @@
 package database
 
 import (
-	"context"
 	"database/sql"
 	"fmt"
 	"go-sandbox/config"
 	"go-sandbox/query"
 	"log"
-	"net"
 
-	"cloud.google.com/go/cloudsqlconn"
 	"github.com/gin-gonic/gin"
-	"github.com/jackc/pgx/v4"
-	"github.com/jackc/pgx/v4/stdlib"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -59,35 +54,11 @@ func connectWithLocalDB() (*gorm.DB, error) {
 }
 
 func connectWithCloudSql() (*gorm.DB, error) {
-	usePrivate := config.Conf.PRIVATE_IP
-	instanceConnectionName := config.Conf.INSTANCE_CONNECTION_NAME
-
-	dsn := fmt.Sprintf("user=%s password=%s database=%s", config.Conf.DbUser, config.Conf.DbPassword, config.Conf.DbName)
-	connConfig, err := pgx.ParseConfig(dsn) //NOTE: pgxはpostgresql用のDriver
+	dsn := fmt.Sprintf("host=%s user=%s password=%s port=%s database=%s", config.Conf.DbHost, config.Conf.DbUser, config.Conf.DbPassword, config.Conf.DbPort, config.Conf.DbName)
+	dbPool, err := sql.Open("pgx", dsn)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("sql.Open: %w", err)
 	}
-	connConfig.DialFunc = func(ctx context.Context, network, instance string) (net.Conn, error) {
-		if usePrivate != "" {
-			d, err := cloudsqlconn.NewDialer(
-				ctx,
-				cloudsqlconn.WithDefaultDialOptions(cloudsqlconn.WithPrivateIP()),
-			)
-			if err != nil {
-				return nil, err
-			}
-			return d.Dial(ctx, instanceConnectionName)
-		}
-		// Use the Cloud SQL connector to handle connecting to the instance.
-		// This approach does *NOT* require the Cloud SQL proxy.
-		d, err := cloudsqlconn.NewDialer(ctx)
-		if err != nil {
-			return nil, err
-		}
-		return d.Dial(ctx, instanceConnectionName)
-	}
-	dbURI := stdlib.RegisterConnConfig(connConfig)
-	dbPool, err := sql.Open("pgx", dbURI)
 	if err != nil {
 		return nil, fmt.Errorf("sql.Open: %v", err)
 	}
