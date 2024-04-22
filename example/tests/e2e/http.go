af533122829d10f14c319ed6ff7baa601fb60c11
Tishida <archersparadox.100@gmail.com>
2024-04-22 21:49:07 +0900

    http test

--- example/tests/e2e/http.go
+++ example/tests/e2e/http.go
@@ -13,6 +13,9 @@ func main() {
 	router.GET("/", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, "Hello, World!")
 	})
+	router.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, "pong")
+	})
 	fmt.Println("Listen on http://localhost:8080")
 	router.Run()
 }
--- example/tests/io/main_test.go
+++ example/tests/io/main_test.go
@@ -3,6 +3,9 @@ package main
 import (
 	"bytes"
 	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -29,4 +32,33 @@ func TestDouble(t *testing.T) {
 
 	expected := []byte("foo\nfoo\n")
 	assert.Equal(t, expected, stdout.Bytes())
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("X-API-VERSION", "1.0")
+	b, _ := io.ReadAll(r.Body)
+	// helloをbodyと連結させる
+	w.Write(append([]byte("hello"), b...))
+	w.WriteHeader(http.StatusOK)
+}
+
+func TestHandler(t *testing.T) {
+	b := strings.NewReader("foo\n")
+	req := httptest.NewRequest(http.MethodGet, "http://localhost/ping", b)
+
+	w := httptest.NewRecorder()
+
+	Handler(w, req)
+
+	if got := w.Header().Get("X-API-VERSION"); got != "1.0" {
+		t.Errorf("X-API-VERSION = %q, want %q", got, "1.0")
+	}
+
+	// body, _ := io.ReadAll(w.Body)
+	// if got := string(body); got != "hellofoo\n" {
+	// 	t.Errorf("body = %q, want %q", got, "hellofoo\n")
+	// }
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, http.StatusOK, w.Result().StatusCode)
 }
