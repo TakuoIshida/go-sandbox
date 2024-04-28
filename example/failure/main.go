3dfbb464c3645bc9d685aa817a5cc22118f3fdb5
Tishida <archersparadox.100@gmail.com>
2024-04-28 21:45:14 +0900

    middleware

--- example/failure/main.go
+++ example/failure/main.go
@@ -3,15 +3,25 @@ package main
 import (
 	"fmt"
 	"go-sandbox/example/failure/handler"
+	"log"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.New()
+	router.Use(someMiddleware()) // ここで認証を行う
 	router.GET("/ping", handler.UserHandleFunc)
 	router.POST("/file/upload", handler.UploadFileHandleFunc)
 	router.GET("/file/download", handler.ReadFileHandleFunc)
 	fmt.Println("Listen on http://localhost:8080")
 	router.Run()
+}
+
+func someMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		log.Println("before request")
+		c.Next()
+		log.Println("after request")
+	}
 }
