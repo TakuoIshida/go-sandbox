cbae74a77aa349fe3cacfcc9390f1b2c3d32dc43
TakuoIshida <archersparadox.100@gmail.com>
2024-10-10 14:25:40 +0900

    ulid make

--- example/ulid-gen/main.go
+++ example/ulid-gen/main.go
@@ -1,33 +1,12 @@
 package main
 
 import (
-	"errors"
-	"math/rand"
+	"fmt"
 
 	"github.com/oklog/ulid/v2"
-
-	"time"
 )
-
-var (
-	t       = time.Unix(1000000, 0)
-	entropy = ulid.Monotonic(rand.New(rand.NewSource(t.UnixNano())), 0)
-)
-
-// generateUlid ex. output: 0000XSNJG0MQJHBF4QX1EFD6Y3
-func generateUlid() ulid.ULID {
-	return ulid.MustNew(ulid.Timestamp(t), entropy)
-}
 
 // Get ex. output: 0000XSNJG0MQJHBF4QX1EFD6Y3
-func Get() string {
-	return generateUlid().String()
-}
-
-// Validation ulid string check size
-func Validation(id string) error {
-	if len(id) != ulid.EncodedSize {
-		return errors.New("Unique ID generator validation error: length is not match")
-	}
-	return nil
+func main() {
+	fmt.Println(ulid.Make())
 }
